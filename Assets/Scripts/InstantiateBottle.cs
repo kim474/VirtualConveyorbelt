f4bee2ee96c8c754732b2c62ff5ab7a27cb517e4
JoohyunLim <jjuha_@naver.com>
2021-05-02 12:53:51 +0900

    first commit

---
+++ Assets/Scripts/InstantiateBottle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateBottle : MonoBehaviour
+{
+    public GameObject Bottle;
+    public float Timer = 3;
+
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0f)
+        {
+            Instantiate(Bottle, new Vector3(1, 1, 0), Quaternion.identity);
+            Timer = 3f;
+        }
+    }
+}
