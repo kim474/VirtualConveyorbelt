6d6e9125acb65f1d36b1ef8098c2a8e26c91f858
JoohyunLim <jjuha_@naver.com>
2021-05-15 01:41:53 +0900

    배경작업, bottle instantiate 수정

--- Assets/Scripts/InstantiateBottle.cs
+++ Assets/Scripts/InstantiateBottle.cs
@@ -17,7 +17,11 @@ public class InstantiateBottle : MonoBehaviour
         Timer -= Time.deltaTime;
         if (Timer <= 0f)
         {
-            Instantiate(Bottle, new Vector3(0.1f, 1.3f, -7.0f), Quaternion.identity);
+            //  Instantiate(Bottle, new Vector3(0.1f, 1.3f, -7.0f), Quaternion.identity);
+            float xpos = Random.Range(-0.2f, 0.2f);
+            float angle = Random.Range(-60f, 60f);
+            Quaternion qRotation = Quaternion.Euler(0f, 0f, angle);
+            Instantiate(Bottle, new Vector3(xpos, 1.3f, -7.0f), qRotation);
             Timer = 3f;
         }
     }
