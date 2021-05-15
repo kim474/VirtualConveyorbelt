fe70df0f57d303a3cabdd26560c851b17ceb9f70
JoohyunLim <jjuha_@naver.com>
2021-05-15 15:59:53 +0900

    main scene 보완

--- Assets/Scripts/InstantiateBottle.cs
+++ Assets/Scripts/InstantiateBottle.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class InstantiateBottle : MonoBehaviour
 {
     public GameObject Bottle;
-    public float Timer = 3;
+    public GameObject Bottle_labeled;
+    List<GameObject> Bottles = new List<GameObject>();
 
+    public float Timer = 3;
 
     void Start()
     {
+        Bottles.Add(Bottle);
+        Bottles.Add(Bottle_labeled);
     }
 
     void Update()
@@ -17,11 +21,12 @@ public class InstantiateBottle : MonoBehaviour
         Timer -= Time.deltaTime;
         if (Timer <= 0f)
         {
-            //  Instantiate(Bottle, new Vector3(0.1f, 1.3f, -7.0f), Quaternion.identity);
             float xpos = Random.Range(-0.2f, 0.2f);
-            float angle = Random.Range(-60f, 60f);
-            Quaternion qRotation = Quaternion.Euler(0f, 0f, angle);
-            Instantiate(Bottle, new Vector3(xpos, 1.3f, -7.0f), qRotation);
+            float angle = Random.Range(-50f, 50f);
+            int bottleIndex = Random.Range(0, 2);
+
+            Quaternion qRotation = Quaternion.Euler(angle, angle, angle);
+            Instantiate(Bottles[bottleIndex], new Vector3(xpos, 1.3f, -7.0f), qRotation);
             Timer = 3f;
         }
     }
